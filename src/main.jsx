047edf9b6ee09d277896c848245b5608bfa5aad7
muhammadbasitobaid <muhammadbasitobaid@gmail.com>
2023-02-21 22:46:21 +0500

    dependencies installation, initial project routes setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './components/Home'
+import Login from './components/Login'
+import Register from './components/Register'
 import App from './App'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+    errorElement: <h1>Page not found</h1>,
+  },
+  {
+    path: 'login/',
+    element: <Login/>
+  },
+  {
+    path: 'register/',
+    element: <Register/>
+  },
+
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+ <RouterProvider router={router}/> 
   </React.StrictMode>,
 )
